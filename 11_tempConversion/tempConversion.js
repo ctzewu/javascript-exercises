19fab91b7ded43ed977f372a078eb5d6ce0cf8a1
ctw <chuatzewu7@gmail.com>
2025-06-19 17:22:29 +0800

    complete /11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(ftemp) {
+    let ctemp = (ftemp - 32)/(9/5)
+    return Math.round(ctemp * 10)/10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(ctemp) {
+    let ftemp = (ctemp*9/5)+32
+    return Math.round(ftemp * 10)/10
 };
 
 // Do not edit below this line
